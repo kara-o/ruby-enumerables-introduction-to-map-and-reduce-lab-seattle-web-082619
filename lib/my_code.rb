e88bcb491e619db0978e84f8dbd7e1ae59f574e9
Kara Odle <karajo.odle@gmail.com>
2019-08-25 20:50:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,6 @@ def reduce_to_total(array, starting_pt = 0)
   array.reduce(starting_pt) { |sum, x| sum + x }
 end
 
-[true, false, true, true]
 
 def reduce_to_all_true(array)
   array.reduce do |memo, element|
